879a571fbb4cf0c7129d75ec860fa4b45b6da957
thom899g <153515950+thom899g@users.noreply.github.com>
2026-02-27 03:17:32 +0100

    Automated commit by HiveMind

---
+++ amltecore__init__.py
@@ -0,0 +1,6 @@
+"""
+Autonomous Meta-Learning Trading Engine Core Module
+Primary architectural entry point for the AMLTE system
+"""
+__version__ = "1.0.0"
+__all__ = ["MetaLearner", "TradingAgent", "StrategyEngine", "StateManager"]
